415a0b017ecfc45c0a27e4dfaa50c2573f093e84
gabceleri1021 <Gabriel.Pedreiro.Senai404@gmail.com>
2024-10-25 10:36:47 -0300

    Add files via upload

---
+++ app_help_desk/app_help_desk/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de Música grátis</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+    <center>
+        <div id="Header">
+            <h1>Bem vindo ao começo do sucesso!</h1>
+            <button class="btn-login" onclick="location.href='entrar.php'">Login</button>
+            <br>
+            <h2>Aqui disponibilizamos cursos gratuitos de qualquer instrumento musical!</h2>
+            <br>
+        </div>
+    </center>
+    <br><br><br>
+    <img id="img" src="cultura-erudita (1).webp" alt="" height="600" width="600">
+    <br><br><br>
+    <div id="fale_conosco">
+        <form action="">
+            <h2 id="mensagem1">Primeiro Passo, Insira o que se pede!</h2>
+    </div>
+    <br>
+    <h3 class="nome_campo">Nome:</h3>
+    <input id="campo" type="nome" placeholder="Digite seu nome aqui">
+    <br><br>
+    <h3 class="nome_campo">Email:</h3>
+    <input id="campo" type="email" placeholder="Digite seu email aqui">
+    <br>
+    <input id="botao" type="submit" value="Enviar!">
+    </form>
+
+    </div>
+    <div id="Footer"></div>
+    <br>
+
+
+
+</body>
+
+</html>
---
+++ app_help_desk/app_help_desk/login.php
@@ -0,0 +1,10 @@
+<?php 
+session_start();
+
+//echo $_SESSION['autenticado'];
+if(!isset($_SESSION['autenticado']) || $_SESSION ['autenticado'] != 'SIM'){
+  header('Location: entrar.php?login=erro2');
+
+}
+
+?>
